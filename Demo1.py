7c4a4e30f3ef17b9c3747b3a8ca437a73aaa20ab
LeoChen <chenleo_tz@163.com>
2019-06-02 16:40:52 +0800

    Add a  timesleep func

---
+++ Demo1.py
@@ -0,0 +1,18 @@
+#! python2
+# coding:utf-8
+# ==============================================================================
+#    Copyright (@) Leo_Chen. All rights reserved.
+#
+#    ModuleName:
+#            .py
+#
+#    Abstract:
+#            
+#    Author:
+#            
+#    Revision History:
+# ==============================================================================
+
+import time
+
+time.sleep(12)
